d787656103164ce18c09ddb5c6e37abb1617230c
Chris Masters <chrismasters@gmail.com>
2024-11-24 16:03:28 +0100

    With working slideshow

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,7 +6,7 @@ import ResponsiveNavigation from "@/components/responsive-navigation";
 
 export default function Navigation() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 container mx-auto">
+    <div className="fixed top-0 left-0 right-0 z-50 container mx-auto yarl__no_scroll_padding">
       <section className="flex justify-between items-center p-4">
         <Link href="/#accueil">
           <ArbaLogo className="fill-black w-20 h-auto" />
--- components/photo-gallery.tsx
+++ components/photo-gallery.tsx
@@ -1,12 +1,58 @@
+"use client";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
 import { RowsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/rows.css";
 
 import Realisation from "@/types/realisation";
+import { useState } from "react";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 export default function PhotoGallery({
   photos = [],
 }: {
   photos: Realisation["photos"];
 }) {
-  return <RowsPhotoAlbum photos={photos} />;
+  const [index, setIndex] = useState(-1);
+  const isMobile = useMediaQuery("(max-width: 1023px)");
+
+  const padding = isMobile ? "16px" : "64px";
+  return (
+    <>
+      <RowsPhotoAlbum
+        photos={photos}
+        onClick={({ index: current }) => setIndex(current)}
+      />
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        carousel={{ padding }}
+        styles={{
+          container: {
+            backgroundColor: "rgba(255, 255, 255, 1)",
+          },
+          button: {
+            backgroundColor: "rgba(255, 255, 255, 0)",
+            filter: "none",
+            color: "rgba(0, 0, 0, 1)",
+          },
+          navigationPrev: {
+            top: "auto",
+            transform: "none",
+            bottom: "0",
+          },
+          navigationNext: {
+            top: "auto",
+            transform: "none",
+            bottom: "0",
+          },
+        }}
+      />
+    </>
+  );
 }
